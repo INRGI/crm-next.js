83209a734f0021f1a5d918469730c639f18d75ee
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-08-12 21:11:10 +0200

    Add company page components

---
+++ src/app/companies/[...id]/page.tsx
@@ -0,0 +1,15 @@
+import Header from '@/app/components/header';
+import React from 'react';
+
+export interface PageProps {
+    params: {id:string[]}
+}
+
+
+export default function Page({params}: PageProps) {
+  return (
+    <>
+      <Header>Companies({params.id})</Header>
+    </>
+  );
+}
